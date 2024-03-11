cf4e86d8334244e2f553e99695619509c39a90ce
manoranjith-shankar <21csbs023@skcet.ac.in>
2024-03-11 09:01:20 +0530

    Add @vercel/analytics package and initialize Analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MyRouts from "./routes/routes"
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/react"
 
 function App(){
   return (
@@ -9,6 +10,7 @@ function App(){
         position="bottom-right"
         reverseOrder={false}
       />
+      <Analytics />
     </div>
   )
 }
